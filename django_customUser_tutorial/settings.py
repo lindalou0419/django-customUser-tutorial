28f03e0b09cda4cd1e1d3fed4d7c5fadac86d666
ilyana19 <aries_2152@hotmail.com>
2019-11-15 17:47:39 -0500

    created new page 'app' and templates to display a static welcome page

--- django_customUser_tutorial/settings.py
+++ django_customUser_tutorial/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
   'django.contrib.sessions',
   'django.contrib.messages',
   'django.contrib.staticfiles',
+
+  'pages',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'django_customUser_tutorial.urls'
 TEMPLATES = [
   {
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [],
+    'DIRS': [os.path.join(BASE_DIR, 'templates')],
     'APP_DIRS': True,
     'OPTIONS': {
       'context_processors': [
