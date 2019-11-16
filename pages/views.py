18455b545ed8c7092c6227beef62d806fe8ec662
ilyana19 <aries_2152@hotmail.com>
2019-11-15 20:54:43 -0500

    added call to action link for signup/login on their respective page

--- pages/views.py
+++ pages/views.py
@@ -4,10 +4,10 @@ from django.views.generic import TemplateView
 # from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 
+def root(request):
+  return redirect('login')
+
+
 @method_decorator(login_required, name='dispatch')
 class HomePageView(TemplateView):
   template_name = 'index.html'
-
-
-def root(request):
-  return redirect('login')
